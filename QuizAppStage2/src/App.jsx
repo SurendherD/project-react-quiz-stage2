fbbc5dbaf033cdfe49d219a3e14949299ce3dd3a
SurendherReddy <surendher4423h@gmail.com>
2024-05-18 15:48:26 +0530

    completedStage2

---
+++ QuizAppStage2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import HomeComponent from './Components/HomeComponent';
+import QuizComponent from './Components/QuizComponent';
+
+function App() {
+  return (
+    <div>
+      {/*<HomeComponent/>*/}
+      <QuizComponent/>
+    </div>
+  )
+}
+
+export default App
